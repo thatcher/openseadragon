92aa9c9ad062d65ea54bf0454150bf28ca6a2d88
thatcher <thatcher.christopher@gmail.com>
2012-06-05 07:52:27 -0400

    Substantial rework of TileSource and Viewer.openTileSource to allow simpler introspect of the intended TileSource implementation based on the configuration object details.  Also major www updates to simplify and unify the informational site by building it via simple templates.  Improved examples and documentation.  Added support for xml, json, jsonp, and inline configuration of supported tile sources.

--- examples/images/highsmith/highsmith.js
+++ examples/images/highsmith/highsmith.js
@@ -1,11 +1,12 @@
-highsmith_dzi({
-	"Image": {
-		"Format":   "jpg", 
-		"Overlap":  "2", 
-		"TileSize": "256",
-		"Size":{ 
-			"Height": "9221",
-			"Width": "7026"
+highsmith({
+	Image: {
+        xmlns:    'http://schemas.microsoft.com/deepzoom/2008',
+		Format:   'jpg',
+		Overlap:  2, 
+		TileSize: 256,
+		Size:{ 
+			Height: 9221,
+			Width:  7026
 		}
 	}
 });
