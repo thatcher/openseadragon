8a043d44b13cfa667438e7dddc51ee87486a619f
thatcher <thatcher.christopher@gmail.com>
2012-06-05 07:52:58 -0400

    Substantial rework of TileSource and Viewer.openTileSource to allow simpler introspect of the intended TileSource implementation based on the configuration object details.  Also major www updates to simplify and unify the informational site by building it via simple templates.  Improved examples and documentation.  Added support for xml, json, jsonp, and inline configuration of supported tile sources.

---
+++ examples/images/rbc/rbc0001/2003/rosen.js
@@ -0,0 +1,16 @@
+rosen({
+	type: 'legacy-image-pyramid',
+	levels:[{
+	    url: '/openseadragon/examples/images/rbc/rbc0001/2003/2003rosen1799/0001q.jpg',
+	    height: 889,
+	    width:  600
+	},{
+	    url: '/openseadragon/examples/images/rbc/rbc0001/2003/2003rosen1799/0001r.jpg',
+	    height: 2201,
+	    width:  1485
+	},{
+	    url: '/openseadragon/examples/images/rbc/rbc0001/2003/2003rosen1799/0001v.jpg',
+	    height: 4402,
+	    width:  2970
+	}]
+});
